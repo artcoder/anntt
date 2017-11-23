8853206f8189ff6000bb3475c5c336eb3e179ded
artcoder <artcoder@gmail.com>
2017-11-23 07:59:25 -0600

    Fixed the visualization, added plot graphic
    Fixed the visualization of the prediction versus the target. Added a sample plot of the prediction versus the target.

--- code_tutorial/estimate_sine_by_shape.py
+++ code_tutorial/estimate_sine_by_shape.py
@@ -1,6 +1,6 @@
-# Python 3
+# for Python 3.6
 #
-# Estimate sine by shape
+# Estimate Sine by Shape
 #
 # Inspired by code from a tutorial at:
 # https://www.datacamp.com/community/tutorials/deep-learning-python
@@ -12,6 +12,9 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 
+EPOCHS = 10000
+BATCH_SIZE = 100
+
 # Using the form: y = f(x)
 # x is the input
 # y is the output
@@ -19,19 +22,22 @@ from keras.layers import Dense
 # Make the input data
 raw_data = [ math.sin(i) for i in np.arange(0.0, 10, 0.01) ]
 time = np.arange(0.0, 10, 0.01)
-# raw_data = [ [math.sin(i), i] for i in np.arange(0.0, 10, 0.01) ]
 
 width = 10
 x = []
 y = []
 for i in range(0, len(raw_data)-width-1):
     #input data
+    # the item x[i] is "width" consecutive outputs of sine(i)
     x.append(raw_data[i:i+width])
+
     #output data
+    # y is the value of sine(i) following the last one in x[i]
     y.append([raw_data[i+width]])
-print("x:", x[0:5])
-print("y:", y[0:5])
-print("time:", time[0:5])
+
+#print("x:", x[0:5])
+#print("y:", y[0:5])
+#print("time:", time[0:5])
 
 # np.random.shuffle(x)
 # print("x:", x[0:10])
@@ -44,7 +50,7 @@ print("time:", time[0:5])
 x_train, x_test, y_train, y_test = train_test_split(x, y)
 # print("length of x_train:", len(x_train))
 
-# Keras model
+# start a Keras model
 model = Sequential()
 
 # Add an the input layer and first layer in one step
@@ -59,36 +65,30 @@ model.add(Dense(100, activation='tanh'))
 # Add the output layer
 model.add(Dense(1, activation='tanh'))
 
-
 model.compile(loss='mean_absolute_error', optimizer='adam',
               metrics=['mean_squared_error'])
 
-EPOCHS = 100
-BATCH_SIZE = 100
 history = model.fit(x_train, y_train, epochs=EPOCHS,
                     batch_size=BATCH_SIZE, verbose=0)
 # plt.plot(history.history['mean_squared_error'])
 # plt.draw()
 
-y_pred = model.predict(x_train)
+#y_pred = model.predict(x_train)
 
-# print("x:", x[0:10])
-# print("y_pred:", y_pred[0:10])
-# print("y:", y[0:10])
+score = model.evaluate(x_test, y_test, batch_size=1, verbose=1)
+print("\nError score:", score)
 
-# fig1 = plt.figure(1)
+y_pred = model.predict(x)
 
-plt.title('Neural Network Estimation')
+
+plt.title('Neural Network Estimation by Shape')
 
 plt.plot(time[0:len(y_pred)], y_pred, label='Prediction',
          color='red', marker='.', linestyle='None')
-plt.plot(time, raw_data, label='Target', color='green', marker=',', linestyle='None')
+plt.plot(time[0:len(y_pred)], y[0:len(y_pred)], label='Target', color='green', marker=',', linestyle='None')
 plt.legend()
-plt.text(5.5, -.9, 'Epochs={}, Batch size={}'.format(EPOCHS, BATCH_SIZE))
+plt.text(5.3, -.98, 'Epochs={}, Batch size={}'.format(EPOCHS, BATCH_SIZE))
 plt.draw()
-
-score=model.evaluate(x_test, y_test, batch_size=1, verbose=1)
-print("\nError score:", score)
 
 # Make a blocking call to keep the pyplot window open
 plt.show()
