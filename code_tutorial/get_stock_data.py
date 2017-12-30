e7f39479009d5d3d8ef3be82b8823a3aefa7900a
David Guilbeau <artcoder@gmail.com>
2017-12-30 14:00:51 -0600

    Clear the session cache of old entries at the start of the run.

--- code_tutorial/get_stock_data.py
+++ code_tutorial/get_stock_data.py
@@ -8,12 +8,15 @@ import pandas_datareader as pdr
 import datetime
 import requests_cache
 
-expire_after = datetime.timedelta(days=3)
+expire_after = datetime.timedelta(days=30)
 session = requests_cache.CachedSession(cache_name='cache', backend='sqlite', expire_after=expire_after)
+
+# housekeeping
+session.cache.remove_old_entries(datetime.datetime.utcnow() - expire_after)
 
 start = datetime.datetime(2010, 1, 1)
 end = datetime.datetime(2013, 1, 27)
 
 f = pdr.data.DataReader("F", 'yahoo', start, end, session=session)
 
-print (f.ix['2010-01-04'])
+print(f.ix['2010-01-04'])
