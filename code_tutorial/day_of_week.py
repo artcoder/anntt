7c5f729130e45722bd6c1d88fd36aa0834cf1760
David Guilbeau <artcoder@gmail.com>
2018-01-17 21:19:29 -0600

    day of the week conversion work

--- code_tutorial/day_of_week.py
+++ code_tutorial/day_of_week.py
@@ -6,14 +6,14 @@ import datetime
 dow = {0: 'Monday', 1: 'Tuesday', 2:'Wednesday', 3:'Thursday',
 4:'Friday', 5:'Saturday', 6:'Sunday'}
 
-dow2 = {0: [1,0,0,0,0], 1:[0,1,0,0,0], 2:[0,0,1,0,0], 3:[0,0,0,1,0],
+dow_expanded = {0: [1,0,0,0,0], 1:[0,1,0,0,0], 2:[0,0,1,0,0], 3:[0,0,0,1,0],
 4:[0,0,0,0,1]}
 
-d = datetime.date(2018, 1, 16)
+d = datetime.date(2018, 1, 18)
 
 wd = d.weekday()
 
 print(wd)
 print(dow[wd])
-print(dow2[wd])
+print(dow_expanded[wd])
 
--- code_tutorial/make_training_data.py
+++ code_tutorial/make_training_data.py
@@ -3,6 +3,7 @@
 
 import datetime
 import pandas_datareader as pdr
+import pandas as pd
 import requests_cache
 
 expire_after = datetime.timedelta(days=30)
@@ -11,18 +12,21 @@ session = requests_cache.CachedSession(cache_name='cache', backend='sqlite', exp
 # housekeeping
 #session.cache.remove_old_entries(datetime.datetime.utcnow() - expire_after)
 
+dow_expanded = {0: [1,0,0,0,0], 1:[0,1,0,0,0], 2:[0,0,1,0,0], 3:[0,0,0,1,0],
+4:[0,0,0,0,1]}
+
 start = datetime.datetime(2010, 1, 1)
 end = datetime.datetime(2013, 1, 27)
 
 f = pdr.data.DataReader("F", 'yahoo', start, end, session=session)
 
-print(f.ix['2010-01-04'])
+#print(f.ix['2010-01-04'])
 
-#print(f.dtypes)
+f['dow_code'] = pd.to_datetime(f.index.values).dayofweek
+f['dow'] = f['dow_code'].apply(lambda x: dow_expanded[x])
 
+print(f.dtypes)
 #print( f.columns)
 
-print(f.head(1))
+print(f.head(5))
 
-datetime.datetime.today()
-print( datetime.datetime.today().weekday() )
